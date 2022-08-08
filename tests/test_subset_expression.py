0b286ebe05baad18cd0f24b0f7bc99798ed0e7a2
Alessandro Pasotti <elpaso@itopen.it>
2022-08-08 12:38:03 +0200

    flake

--- tests/test_subset_expression.py
+++ tests/test_subset_expression.py
@@ -12,19 +12,23 @@ __author__ = 'elpaso@itopen.it'
 __date__ = '2022-08-08'
 __copyright__ = 'Copyright 2022, ItOpen'
 
-import os, sys, importlib.util
+import os
+import sys
+import importlib.util
 from pathlib import Path
-from osgeo import gdal, ogr
-from unittest import main, skipIf
-from qgis.core import QgsApplication, QgsVectorLayer, QgsProject, QgsExpressionContextUtils
+from osgeo import ogr
+from unittest import main
+from qgis.core import QgsVectorLayer, QgsProject, QgsExpressionContextUtils
 from qgis.PyQt.QtCore import QTemporaryDir
 from qgis.testing import TestCase, start_app
 from qgis.testing.mocked import get_iface
 from qgis import utils as qgis_utils
 
-sys.path.append(str(Path(os.path.dirname(__file__)).parent.absolute().parent.absolute()))
+sys.path.append(
+    str(Path(os.path.dirname(__file__)).parent.absolute().parent.absolute()))
 
 QGISAPP = start_app()
+
 
 class SubsetExpressionUtilsTest(TestCase):
 
@@ -32,16 +36,19 @@ class SubsetExpressionUtilsTest(TestCase):
     def setUpClass(cls):
 
         cls.iface = get_iface()
-        qgis_utils.plugin_paths = [str(Path(os.path.dirname(__file__)).parent.absolute().parent.absolute())]
+        qgis_utils.plugin_paths = [
+            str(Path(os.path.dirname(__file__)).parent.absolute().parent.absolute())]
         qgis_utils.updateAvailablePlugins()
         result = qgis_utils.loadPlugin('qgis-subset-expression-plugin')
         assert result
         package = sys.modules['qgis-subset-expression-plugin']
-        qgis_utils.plugins['qgis-subset-expression-plugin'] = package.classFactory(cls.iface)
+        qgis_utils.plugins['qgis-subset-expression-plugin'] = package.classFactory(
+            cls.iface)
         assert result
         cls.package = package
 
-        spec = importlib.util.spec_from_file_location("utils", os.path.join(package.__path__[0], "utils.py"))
+        spec = importlib.util.spec_from_file_location(
+            "utils", os.path.join(package.__path__[0], "utils.py"))
         utils = importlib.util.module_from_spec(spec)
         sys.modules["utils"] = utils
         globals()['utils'] = utils
@@ -56,8 +63,10 @@ class SubsetExpressionUtilsTest(TestCase):
         """Prepare test data and project"""
 
         self.tmp_dir = QTemporaryDir()
-        self.temp_path = os.path.join(self.tmp_dir.path(), 'subset_expression_test.gpkg')
-        self.temp_project_path = os.path.join(self.tmp_dir.path(), 'subset_expression_test.qgs')
+        self.temp_path = os.path.join(
+            self.tmp_dir.path(), 'subset_expression_test.gpkg')
+        self.temp_project_path = os.path.join(
+            self.tmp_dir.path(), 'subset_expression_test.qgs')
 
         ds = ogr.GetDriverByName('GPKG').CreateDataSource(self.temp_path)
         lyr = ds.CreateLayer('test_layer', geom_type=ogr.wkbNone)
@@ -75,7 +84,8 @@ class SubsetExpressionUtilsTest(TestCase):
         f = None
         ds = None
 
-        test_layer = QgsVectorLayer(self.temp_path + '|layername=test_layer', 'test_layer')
+        test_layer = QgsVectorLayer(
+            self.temp_path + '|layername=test_layer', 'test_layer')
         QgsProject.instance().addMapLayers([test_layer])
         self.layer = test_layer
 
@@ -84,28 +94,35 @@ class SubsetExpressionUtilsTest(TestCase):
 
     def test_subset_expression(self):
 
-        self.assertEqual(set([f['name'] for f in self.layer.getFeatures()]), {'A1', 'A2', 'B1'})
-        utils.store_subset_expression(self.layer, "name LIKE '@first_letter%'", True, self.iface)
+        self.assertEqual(set([f['name'] for f in self.layer.getFeatures()]), {
+                         'A1', 'A2', 'B1'})
+        utils.store_subset_expression(self.layer, "name LIKE '@first_letter%'", True, self.iface)  # noqa: F821
 
         # There is no variable set, no expression is set
-        self.assertEqual(set([f['name'] for f in self.layer.getFeatures()]), {'A1', 'A2', 'B1'})
+        self.assertEqual(set([f['name'] for f in self.layer.getFeatures()]), {
+                         'A1', 'A2', 'B1'})
 
         # Set the expression at project level
-        QgsExpressionContextUtils.setProjectVariable(QgsProject.instance(), 'first_letter','A')
-        self.assertEqual(set([f['name'] for f in self.layer.getFeatures()]), {'A1', 'A2'})
+        QgsExpressionContextUtils.setProjectVariable(
+            QgsProject.instance(), 'first_letter', 'A')
+        self.assertEqual(set([f['name']
+                              for f in self.layer.getFeatures()]), {'A1', 'A2'})
 
-        QgsExpressionContextUtils.setGlobalVariable('first_letter','B')
+        QgsExpressionContextUtils.setGlobalVariable('first_letter', 'B')
         # No changes so far: project has precedence
-        self.assertEqual(set([f['name'] for f in self.layer.getFeatures()]), {'A1', 'A2'})
+        self.assertEqual(set([f['name']
+                              for f in self.layer.getFeatures()]), {'A1', 'A2'})
 
         # Now we get B!
-        QgsExpressionContextUtils.removeProjectVariable(QgsProject.instance(), 'first_letter')
-        self.assertEqual(set([f['name'] for f in self.layer.getFeatures()]), {'B1'})
+        QgsExpressionContextUtils.removeProjectVariable(
+            QgsProject.instance(), 'first_letter')
+        self.assertEqual(set([f['name']
+                              for f in self.layer.getFeatures()]), {'B1'})
 
         # Clear, no var, no changes!
         QgsExpressionContextUtils.removeGlobalVariable('first_letter')
-        self.assertEqual(set([f['name'] for f in self.layer.getFeatures()]), {'B1'})
-
+        self.assertEqual(set([f['name']
+                              for f in self.layer.getFeatures()]), {'B1'})
 
 
 if __name__ == '__main__':
